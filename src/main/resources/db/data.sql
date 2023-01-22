c82b169b0333f6618070d539620b3b0c2f320f11
clean17 <piw940317@gmail.com>
2023-01-22 11:04:56 +0900

    1차 완성.2

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,7 +4,7 @@ insert into user_tb(username, password, email, created_at) values('love', '1234'
 
 insert into product_tb(name, price, qty, created_at) values('바나나', 1000, 97, now());
 insert into product_tb(name, price, qty, created_at) values('사과', 2000, 97, now());
-insert into product_tb(name, price, qty, created_at) values('딸기', 3000, 97, now());
+insert into product_tb(name, price, qty, created_at) values('딸기', 3000, 100, now());
 
 insert into purchase_tb(user_id, product_id, count, created_at) values(1, 1, 1, now());
 insert into purchase_tb(user_id, product_id, count, created_at) values(1, 2, 1, now());
@@ -14,4 +14,3 @@ insert into purchase_tb(user_id, product_id, count, created_at) values(3, 1, 1, 
 insert into purchase_tb(user_id, product_id, count, created_at) values(3, 2, 1, now());
 
 commit;
-
